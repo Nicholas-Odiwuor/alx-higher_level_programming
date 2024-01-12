714417b92188b0ed2383a45a99549c7ad4f11178
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-01-12 03:11:31 -0800

    Lists

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,21 +1,69 @@
-#ifndef LISTS_H
-#define LISTS_H
+#include <stdlib.h>
+
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct listint_s
+
+  * insert_node - inserts a number into a sorted singly linked list.
+
+  * @head: pointer to the pointer of the list.
+
+  * @number: number to be inserted.
+
+  *
+
+  * Return: address f the new node or NULL if it failed.
+
+  */
+
+listint_t *insert_node(listint_t **head, int number)
+
 {
-    int n;
-    struct listint_s *next;
-} listint_t;
 
-/* Function prototype */
-listint_t *insert_node(listint_t **head, int number);
+	listint_t *before, *new_Node;
 
-#endif /* LISTS_H */
 
+
+	new_Node = malloc(sizeof(listint_t));
+
+	if (new_Node != NULL)
+
+	{
+
+		new_Node->n = number;
+
+		if (*head == NULL || (*head)->n >= new_Node->n)
+
+		{
+
+			new_Node->next = *head;
+
+			*head = new_Node;
+
+			return (new_Node);
+
+		}
+
+		else
+
+		{
+
+			before = *head;
+
+			while (before->next != NULL && before->next->n < new_Node->n)
+
+				before = before->next;
+
+			new_Node->next = before->next;
+
+			before->next = new_Node;
+
+			return (new_Node);
+
+		}
+
+	}
+
+	return (NULL);
+
+}
