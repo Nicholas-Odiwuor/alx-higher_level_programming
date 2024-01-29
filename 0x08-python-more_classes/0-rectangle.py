208b80400df66fd112630dd97b917d22db560eaf
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-01-29 08:27:01 -0800

    More classes and objects

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""Empty class Rectangle"""
+"""
+Defines an empty class Rectangle
+"""
 
 
 class Rectangle:
-    """Empty class Rectangle"""
+    """Empty representation of a rectangle"""
     pass
-
