6d46fcf482eb406588e19828dd531a73ffc14f31
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-01-31 01:01:19 -0800

    Test-driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for add_integer function.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first parameter.
+        b (int or float): The second parameter.
+
+    Returns:
+        int: The addition of a and b (casted to integers).
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
