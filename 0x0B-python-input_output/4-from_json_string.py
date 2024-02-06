c55a81dfc9771163dd8977d8726399cb4938e489
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-02-06 01:34:37 -0800

    Input & Output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contain json str function
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """return an object repriesented by  JSON str"""
+    return json.loads(my_str)
