2ea8fd0b6cbbfb3e11666d9efe8cd532235609e4
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-02-06 00:39:18 -0800

    Python Inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+contains the my_List clas
+"""
+
+
+class MyList(list):
+    """subclass of list"""
+    def __init__(self):
+        """initialises the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
