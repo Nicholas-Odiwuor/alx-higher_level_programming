e1d439604d71320fb2a0bae539afbf1c821e62df
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-03-14 09:02:11 -0700

    SQL Intro

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,9 +1,2 @@
--- Script to print the full description of the table first_table
-
-USE hbtn_0c_0; -- mysql
-
-SELECT CONCAT('Table   Create Table                                                                         
-first_table     ', CREATE_TABLE) AS 'Create Table'
-FROM information_schema.TABLES
-WHERE TABLE_NAME = 'first_table';
-i
+-- It prints full description of the table first_table.
+SHOW CREATE TABLE `first_table`;
