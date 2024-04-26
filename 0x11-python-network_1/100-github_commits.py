c6e1b2fc4f96e22c8cd22edf9363039066dfff23
Nicholas-Odiwuor <nickeyphillip155@gmail.com>
2024-04-26 10:02:07 -0700

    Python Network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,23 +1,22 @@
+#!/usr/bin/python3
+"""Script that takes 2 arguments\
+        in order to solve this challenge:list 10 commits\
+        (from the most recent to oldest) of a repository\
+        using use the GitHub API. and Print \
+        all commits by: `<sha>: <author name>` (one by line)"""
 import requests
 import sys
 
-def list_commits(repository, owner):
-    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
-    response = requests.get(url)
-    if response.status_code == 200:
-        commits = response.json()
-        for commit in commits[:10]:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f"{sha}: {author_name}")
-    else:
-        print(f"Error: Failed to fetch commits. Status code: {response.status_code}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <repository> <owner>")
-        sys.exit(1)
-    repository = sys.argv[1]
+    repo = sys.argv[1]
     owner = sys.argv[2]
-    list_commits(repository, owner)
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    response = requests.get(url)
+    json_resp = response.json()
 
+for commit in json_resp[:10]:
+    commit_data = commit['commit']
+    sha = commit_data.get('sha')
+    author = commit_data.get('author').get('name')
+    print(f"{sha}: {author}")
+    pass
